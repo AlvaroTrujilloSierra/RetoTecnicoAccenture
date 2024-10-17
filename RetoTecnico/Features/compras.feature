39a96849de8606c03a5af35e8bcf1ddaa19ff7d0
Oscar Sierra <oscar.sierra@hubse.com>
2024-10-17 12:16:34 -0500

    upload reto tecnico

---
+++ RetoTecnico/Features/compras.feature
@@ -0,0 +1,12 @@
+﻿Feature: Realizar compra en Demoblaze
+
+  Scenario Outline: Comprar un producto de una categoría
+    Given que el usuario se registra con nombre "<nombre>" y contraseña "<contraseña>"
+    When el usuario selecciona la categoría "<categoria>" y el producto "<producto>"
+    And añade el producto al carrito
+    Then el usuario completa la compra
+
+  Examples:
+    | nombre     | contraseña | categoria | producto     |
+    | alvarodaniel10111111   | pass123    | Phones    | Nexus 6  |
+    | alvarodaniel20111111   | pass123    | Laptops   | MacBook air    |
---
+++ RetoTecnico/Steps/CompraSteps.cs
@@ -0,0 +1,57 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using TechTalk.SpecFlow;
+
+namespace RetoTecnico.Steps
+{
+    [Binding]
+    public class CompraSteps
+    {
+        private IWebDriver driver;
+
+        [Given(@"que el usuario se registra con nombre ""(.*)"" y contraseña ""(.*)""")]
+        public void DadoQueElUsuarioSeRegistra(string nombre, string contraseña)
+        {
+            driver = new ChromeDriver(); 
+            driver.Navigate().GoToUrl("https://www.demoblaze.com/#");
+            driver.FindElement(By.Id("signin2")).Click();
+            Thread.Sleep(2000);
+            driver.FindElement(By.Id("sign-username")).SendKeys(nombre);
+            driver.FindElement(By.Id("sign-password")).SendKeys(contraseña);
+            driver.FindElement(By.XPath("//button[text()='Sign up']")).Click();
+            Thread.Sleep(3000);
+            IAlert alert = driver.SwitchTo().Alert();
+            alert.Accept(); 
+            Thread.Sleep(3000);
+        }
+
+        [When(@"el usuario selecciona la categoría ""(.*)"" y el producto ""(.*)""")]
+        public void CuandoElUsuarioSeleccionaCategoriaYProducto(string categoria, string producto)
+        {
+
+            driver.FindElement(By.LinkText(categoria)).Click();
+            Thread.Sleep(2000);
+            driver.FindElement(By.LinkText(producto)).Click();
+            Thread.Sleep(2000);
+        }
+
+        [When(@"añade el producto al carrito")]
+        public void CuandoAñadeElProductoAlCarrito()
+        {
+            driver.FindElement(By.CssSelector(".btn.btn-success")).Click(); 
+            Thread.Sleep(2000);
+        }
+
+        [Then(@"el usuario completa la compra")]
+        public void EntoncesElUsuarioCompletaLaCompra()
+        {
+            IAlert alert = driver.SwitchTo().Alert();
+            alert.Accept(); // Acepta el alert
+            driver.FindElement(By.Id("cartur")).Click();
+            Thread.Sleep(2000);
+            driver.FindElement(By.CssSelector(".btn.btn-success")).Click();
+            Thread.Sleep(2000);
+            driver.Quit(); 
+        }
+    }
+}
